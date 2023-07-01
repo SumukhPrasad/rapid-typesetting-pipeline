c5d4ae9443524b95047f4b093229504a79dac281
Sumukh Prasad <sumukhprasad@icloud.com>
2023-07-01 19:56:32 +0530

    Added createnew.

---
+++ createnew.sh
@@ -0,0 +1,8 @@
+echo "\033[1mPlease specify an absolute path:\033[0m"
+read path
+
+echo "\033[1mCreating directory...\033[0m"
+mkdir $path
+
+echo "\033[1mCopying files...\033[0m"
+rsync -av --exclude=".*" ./* $path
